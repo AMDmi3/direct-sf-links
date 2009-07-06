b8a05d0e18137b82574ad678b9dd6c14d50b183d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2009-07-06 21:59:00 +0300

    Add support for new File Release System

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Direct links on Sourceforge download pages
 // @namespace      http://www.amdmi3.ru/
-// @include        http://sourceforge.net/project/showfiles.php*
-// @include        http://www.sourceforge.net/project/showfiles.php*
+// @include        http://sourceforge.net/projects/*/files/
+// @include        http://www.sourceforge.net/projects/*/files/
 // ==/UserScript==
 
 (function()
@@ -10,17 +10,13 @@
   // !!! Change this to your favorite mirror !!!
   var mirror = "your_favorite_mirror";
 
-  var project = document.evaluate(
-      "//a[@name='projectnav']/following::a[starts-with(@href, '/projects/')]/@href",
-      document, null, XPathResult.STRING_TYPE, null
-    ).stringValue.replace("/projects/", "").replace("/", "");
-
-  for(var i=0; i<document.links.length; i++) {
+  for(var i = 0; i < document.links.length; i++) {
     var elem = document.links[i];
 
-    if (elem.href.match(/\/project\/downloading.php.*filename=([^&]+)/i)) {
-      elem.href="http://"+mirror+".dl.sourceforge.net/sourceforge/"+project+"/"+RegExp.$1;
+    if (elem.href.match(/\/projects\/([^\/]+)\/files\/(.+\/[^&\/]+)\/download/i)) {
+
+      elem.href="http://"+mirror+".dl.sourceforge.net/project/"+RegExp.$1+"/"+RegExp.$2;
+
     }
   }
-
 })();
