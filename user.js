3bea39c84b36206f6b3194cf9b6e024ed71add24
Dmitry Marakasov <amdmi3@amdmi3.ru>
2010-07-28 17:01:00 +0300

    Add copyright and license information

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name           Direct links on Sourceforge download pages
 // @namespace      http://www.amdmi3.ru/
+// @copyright      2009-2010, Dmitry Marakasov <amdmi3@amdmi3.ru>
+// @license        BSD
 // @include        http://sourceforge.net/projects/*
 // @include        http://www.sourceforge.net/projects/*
 // @include        https://sourceforge.net/projects/*
