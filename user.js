a9908d901e0b13c9dbfdd5b1208cd21047f26d78
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-06-24 00:27:00 +0400

    Spaces -> tabs, update copyright

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Direct links on Sourceforge download pages
 // @namespace      http://www.amdmi3.ru/
-// @copyright      2009-2010, Dmitry Marakasov <amdmi3@amdmi3.ru>
+// @copyright      2009-2011, Dmitry Marakasov <amdmi3@amdmi3.ru>
 // @license        BSD
 // @include        http://sourceforge.net/projects/*
 // @include        http://www.sourceforge.net/projects/*
@@ -11,14 +11,14 @@
 
 (function()
 {
-  // !!! Change this to your favorite mirror !!!
-  var mirror = "kent";
+	// !!! Change this to your favorite mirror !!!
+	var mirror = "kent";
 
-  for(var i = 0; i < document.links.length; i++) {
-    var elem = document.links[i];
+	for(var i = 0; i < document.links.length; i++) {
+		var elem = document.links[i];
 
-    if (elem.href.match(/\/projects\/([^\/]+)\/files\/(.+)\/download/i)) {
-      elem.href="http://"+mirror+".dl.sourceforge.net/project/"+RegExp.$1+"/"+RegExp.$2;
-    }
-  }
+		if (elem.href.match(/\/projects\/([^\/]+)\/files\/(.+)\/download/i)) {
+			elem.href="http://"+mirror+".dl.sourceforge.net/project/"+RegExp.$1+"/"+RegExp.$2;
+		}
+	}
 })();
